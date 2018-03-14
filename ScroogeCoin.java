b05150f345ce0f49aecbe77aa9fee4bbf1fc6f7f
Ken Zheng <kzheng@ucdavis.edu>
2018-03-14 01:42:56 -0700

    Created root transaction signed by Scrooge

--- ScroogeCoin.java
+++ ScroogeCoin.java
@@ -1,7 +1,8 @@
 import java.security.*;
+import java.math.BigInteger;
 
 public class ScroogeCoin {
-	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException {
+	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException {
 		// crypto setup
 		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
 		SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
@@ -19,5 +20,22 @@ public class ScroogeCoin {
 		
 		// System.out.println("Scrooge private: " + privateKeyScrooge + " public: " + publicKeyScrooge);
 		// System.out.println("Alice private: " + privateKeyAlice + " public: " + publicKeyAlice);
+		
+		// create root transaction so that Scrooge owns a coin
+		Transaction tx = new Transaction();
+		tx.addOutput(5, publicKeyScrooge);
+		
+		// just create an arbritrary initial hash value 
+		byte[] initialHash = BigInteger.valueOf(543754544).toByteArray();
+		tx.addInput(initialHash, 0);
+		
+		// have Scrooge sign root transaction
+		Signature signature = Signature.getInstance("SHA256withRSA");
+		signature.initSign(privateKeyScrooge);
+		signature.update(tx.getRawDataToSign(0));
+ 		byte[] sig = signature.sign();
+		tx.addSignature(sig, 0);
+		
+		// boolean verified = Crypto.verifySignature(publicKeyScrooge, tx.getRawDataToSign(0), sig);
 	}
 }
