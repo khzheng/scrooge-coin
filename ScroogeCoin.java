7b06005ef984dc960facb0758c123413f396d7f4
Ken Zheng <kzheng@ucdavis.edu>
2018-03-14 01:18:02 -0700

    generated key pairs for Scrooge and Alice

--- ScroogeCoin.java
+++ ScroogeCoin.java
@@ -1,5 +1,23 @@
+import java.security.*;
+
 public class ScroogeCoin {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException {
+		// crypto setup
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+		SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+		keyGen.initialize(1024, random);
 		
+		// generate a keypair for Scrooge
+		KeyPair pair = keyGen.generateKeyPair();
+		PrivateKey privateKeyScrooge = pair.getPrivate();
+		PublicKey publicKeyScrooge = pair.getPublic();
+		
+		// generate a keypair for Alice
+		pair = keyGen.generateKeyPair();
+		PrivateKey privateKeyAlice = pair.getPrivate();
+		PublicKey publicKeyAlice = pair.getPublic();
+		
+		// System.out.println("Scrooge private: " + privateKeyScrooge + " public: " + publicKeyScrooge);
+		// System.out.println("Alice private: " + privateKeyAlice + " public: " + publicKeyAlice);
 	}
 }
